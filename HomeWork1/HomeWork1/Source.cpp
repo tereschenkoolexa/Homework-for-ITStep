e1c49e1db3c1e82e6fe438ccd1950d47f1ba9a54
Pena <sashapena1337@gmail.com>
2018-11-20 16:31:25 +0200

    Completed the task and completed the additional task

--- HomeWork1/HomeWork1/Source.cpp
+++ HomeWork1/HomeWork1/Source.cpp
@@ -2,6 +2,7 @@
 
 using namespace std;
 enum currency { UAH = '1', EUR = '2', DOL = '3', JPY = '4' };
+
 struct account
 {
 
@@ -14,11 +15,12 @@ public:
 
 	void input_card()
 	{
-
+	
 		cout << char(178) << "Number ? : ";
 		cin >> number;
 		cout << char(178) << "Cash ? : ";
 		cin >> cash;
+		cout << char(178) << " UAH[1], EUR[2], DOL[3], JPY[4] " << endl;
 		cout << char(178) << "Curnency ? : ";
 		cin >> currency_IP;
 		switch (currency_IP)
@@ -170,7 +172,7 @@ public:
 
 	}
 
-	void cart_option()
+	void cart_option(account card2)
 	{
 		double amount_of_money;
 		int menu;
@@ -187,13 +189,25 @@ public:
 			break;
 
 		case 2:
+			mitka:
 			system("cls");
 			cout << char(178) << "Count cash ? : ";
 			cin >> amount_of_money;
-
+			if (amount_of_money <= cash)
+			{
 			currency_conversion(currency_IP, amount_of_money);
-			
+
 			cash -= amount_of_money;
+			}
+			else
+			{
+
+				cout << char(178) << "The account does not have enough money, if you need money," << endl;
+				cout << char(178) << "take a loan at 380691337777 or on the CREDIT.ua website" << endl;
+				cout << char(178); system("pause");
+				goto mitka;
+
+			}
 			cout << char(178); system("pause");
 			break;
 
@@ -206,6 +220,232 @@ public:
 		
 			cash += amount_of_money;
 			cout << char(178); system("pause");
+			break;
+		case 4:
+			mmitka:
+			system("cls");
+			int i;
+			cout << char(178) << "Get money [0] || Translate money [1] ";
+			cin >> i;
+			system("cls");
+			cout << char(178) << "Count cash ?" << endl;
+			cout << char(178) << " : ";
+			cin >> amount_of_money;
+			if (i == 0)
+			{
+			
+				if (amount_of_money <= card2.cash)
+				{
+					switch (card2.currency_IP)
+					{
+					case 1:
+						if (currency_IP == 1)
+						{
+							amount_of_money *= 1;
+
+						}
+						if (currency_IP == 2)
+						{
+							amount_of_money *= 0.032;
+						}
+						if (currency_IP == 3)
+						{
+							amount_of_money *= 0.036;
+
+						}
+						if (currency_IP == 4)
+						{
+							amount_of_money *= 4.08;
+
+						}
+
+						break;
+					case 2:
+						if (currency_IP == 1)
+						{
+							amount_of_money *= 31.57;
+
+						}
+						if (currency_IP == 2)
+						{
+							amount_of_money *= 1;
+						}
+						if (currency_IP == 3)
+						{
+							amount_of_money *= 1.15;
+
+						}
+						if (currency_IP == 4)
+						{
+							amount_of_money *= 128.87;
+
+						}
+
+						break;
+					case 3:
+						if (currency_IP == 1)
+						{
+							amount_of_money *= 27.57;
+
+						}
+						if (currency_IP == 2)
+						{
+							amount_of_money *= 0.87;
+						}
+						if (currency_IP == 3)
+						{
+							amount_of_money *= 1;
+
+						}
+						if (currency_IP == 4)
+						{
+							amount_of_money *= 112.55;
+
+						}
+
+						break;
+					case 4:
+						if (currency_IP == 1)
+						{
+							amount_of_money *= 0.24;
+
+						}
+						if (currency_IP == 2)
+						{
+							amount_of_money *= 0.0078;
+						}
+						if (currency_IP == 3)
+						{
+							amount_of_money *= 0.0089;
+
+						}
+						if (currency_IP == 4)
+						{
+							amount_of_money *= 1;
+
+						}
+
+						break;
+					}
+
+					cash += amount_of_money;
+				}
+				else
+					cout << char(178) << " There is no such amount on the second account " << endl;
+			
+			}
+			else if (i == 1)
+			{
+
+				if (amount_of_money <= cash)
+				{
+					switch (card2.currency_IP)
+					{
+					case 1:
+						if (currency_IP == 1)
+						{
+							amount_of_money *= 1;
+
+						}
+						if (currency_IP == 2)
+						{
+							amount_of_money *= 0.032;
+						}
+						if (currency_IP == 3)
+						{
+							amount_of_money *= 0.036;
+
+						}
+						if (currency_IP == 4)
+						{
+							amount_of_money *= 4.08;
+
+						}
+
+						break;
+					case 2:
+						if (currency_IP == 1)
+						{
+							amount_of_money *= 31.57;
+
+						}
+						if (currency_IP == 2)
+						{
+							amount_of_money *= 1;
+						}
+						if (currency_IP == 3)
+						{
+							amount_of_money *= 1.15;
+
+						}
+						if (currency_IP == 4)
+						{
+							amount_of_money *= 128.87;
+
+						}
+
+						break;
+					case 3:
+						if (currency_IP == 1)
+						{
+							amount_of_money *= 27.57;
+
+						}
+						if (currency_IP == 2)
+						{
+							amount_of_money *= 0.87;
+						}
+						if (currency_IP == 3)
+						{
+							amount_of_money *= 1;
+
+						}
+						if (currency_IP == 4)
+						{
+							amount_of_money *= 112.55;
+
+						}
+
+						break;
+					case 4:
+						if (currency_IP == 1)
+						{
+							amount_of_money *= 0.24;
+
+						}
+						if (currency_IP == 2)
+						{
+							amount_of_money *= 0.0078;
+						}
+						if (currency_IP == 3)
+						{
+							amount_of_money *= 0.0089;
+
+						}
+						if (currency_IP == 4)
+						{
+							amount_of_money *= 1;
+
+						}
+
+						break;
+					}
+
+					cash -= amount_of_money;
+				}
+				else
+					cout << char(178) << " There is no such amount on the second account " << endl;
+
+			}
+			else
+			{
+				
+				cout << char(178) << " ERROR , NOT the correct answer " << endl;
+				cout << char(178); system("pause");
+				goto mmitka;
+
+			}
+				cout << char(178); system("pause");
 			break;
 		case 0:
 			exit(0);
@@ -223,23 +463,45 @@ public:
 int main()
 {
 	account card;
-	card.input_card();
+	account card2;
+	mitka:
+	system("cls");
 	cout << char(178) << " Create another one besides the main one?[1]Yes [0]No " << endl;
 	cout << char(178) << " : ";
 	int YN;
 	cin >> YN;
 	if (YN == 1)
 	{
-		account card2;
+		system("cls");
 		card2.input_card();
+		cout << char(178) << " Continue to fill in the main one " << endl;
+		cout << char(178); system("pause");
 	}
+	else if(YN==0)
+	{
+
+		card2.cash = 2000;
+		card2.number = 8080;
+		card2.currency_IP = 2;
+
+	}
+	else
+			{
+
+			cout << char(178) << " ERROR , NOT the correct answer " << endl;
+			cout << char(178); system("pause");
+			goto mitka;
+
+			}
+	system("cls");
+	card.input_card();
 	while (1 != 0)
 	{
 		system("cls");
 		cout << char(178) << "1 Open an account " << "2 Withdraw money from the account    " << char(178) << endl;
-		cout << char(178) << "3 Put money on the account "  << "4  " << char(178) << endl;
+		cout << char(178) << "3 Put money on the account "  << "4 Money transfer            " << char(178) << endl;
 		cout << char(178) << "0 Exit";for(int i=0;i<7;i++)cout << "\t"; cout << char(178) << endl;
-		card.cart_option();
+		card.cart_option(card2);
 	}
 	cout << char(178); system("pause");
 
